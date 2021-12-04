5dd0513d96cc5ab566f33937268d33d5c7f19caa
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-03 21:58:11 -0500

    test script structure complete

--- test/Hotel.test.js
+++ test/Hotel.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+const web3 = new Web3(ganache.provider({ gasLimit: 10000000 }));
 
 const { abi, evm } = require('../ethereum/build/Hotel.json');
 
@@ -15,7 +15,7 @@ beforeEach(async () => {
         data: evm.bytecode.object,
         arguments: [],
       })
-      .send({ from: accounts[0], gas: '1000000' });
+      .send({ from: accounts[0], gas: '10000000' });
 });
 
 describe('Hotel', () => {
