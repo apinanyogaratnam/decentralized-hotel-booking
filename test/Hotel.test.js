3e35fb3bbbc83fb280609ccac5e9bde643c1bc90
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-03 22:10:49 -0500

    completed can get a room test

--- test/Hotel.test.js
+++ test/Hotel.test.js
@@ -8,6 +8,8 @@ const { abi, evm } = require('../ethereum/build/Hotel.json');
 let accounts;
 let hotel;
 
+const typeOfRoom = ['Single', 'Double', 'Suite'];
+
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
     hotel = await new web3.eth.Contract(abi)
@@ -21,5 +23,17 @@ beforeEach(async () => {
 describe('Hotel', () => {
     it('deploys a contract', () => {
         assert.ok(hotel.options.address);
+    });
+
+    it('can get a room', async () => {
+        const room1 = await hotel.methods.getRoom(0).call();
+        
+        assert.equal(20000000000000000, room1[0]); // room price is 20000000000000000 wei
+        assert.equal(0, room1[1]); // room type is single
+        assert.equal(1, room1[2]); // room number is 1
+        assert.equal(false, room1[3]); // room is not occupied
+        assert.equal(0, room1[4]); // starting time is 0
+        assert.equal(0, room1[5]); // ending time is 0
+        assert.equal(0, room1[6]); // owing of room is 0
     });
 });
