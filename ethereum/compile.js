5a6ea5f006d73ba5e25e5053e43da9d84a58f595
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-03 21:47:00 -0500

    completed compile script

---
+++ ethereum/compile.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const fs = require('fs-extra');
+const solc = require('solc');
+
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+const hotelPath = path.resolve(__dirname, 'contracts', 'Hotel.sol');
+const source = fs.readFileSync(hotelPath, 'utf8');
+
+const input = {
+  language: 'Solidity',
+  sources: {
+    'Hotel.sol': {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*'],
+      },
+    },
+  },
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+fs.ensureDirSync(buildPath);
+
+fs.outputJsonSync(
+    path.resolve(buildPath, 'Hotel.json'),
+    output.contracts['Hotel.sol'].MusicList
+);
