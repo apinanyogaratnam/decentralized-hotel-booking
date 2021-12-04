ac460c548e9c127e752c3893eff9cd3838d670da
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-03 21:15:12 -0500

    contract complete

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Decentralized Hotel Booking
+
+notes:
+- frontend checks and restricts if rooms are already occupied
+- there won't be an error for indexes since user clicks on the room which generates and index
+- request room rates in frontend 20000000000000000, 40000000000000000, 60000000000000000
+- add 100 more wei to allow for cost in seconds to not go up
+- need to call checkout and pay at the same time in the frontend
--- ethereum/contracts/Hotel.sol
+++ ethereum/contracts/Hotel.sol
@@ -8,6 +8,9 @@ contract Hotel {
         RoomType typeOfRoom;
         uint roomNumber;
         bool occupied;
+        uint start;
+        uint end;
+        uint owing;
     }
     // Room 1-15 are single rooms
     uint public numberOfSingleRooms;
@@ -25,28 +28,37 @@ contract Hotel {
         for (uint i=0; i<30; i++) {
             if (0 <= i && i < 15) {
                 Room memory room = Room({
-                    price: 100,
+                    price: 20000000000000000,
                     typeOfRoom: RoomType.SINGLE,
                     roomNumber: i + 1,
-                    occupied: false
+                    occupied: false,
+                    start: 0,
+                    end: 0,
+                    owing: 0
                 });
 
                 rooms.push(room);
             } else if (15 <= i && i < 25) {
                 Room memory room = Room({
-                    price: 200,
+                    price: 40000000000000000,
                     typeOfRoom: RoomType.DOUBLE,
                     roomNumber: i + 1,
-                    occupied: false
+                    occupied: false,
+                    start: 0,
+                    end: 0,
+                    owing: 0
                 });
 
                 rooms.push(room);
             } else {
                 Room memory room = Room({
-                    price: 300,
+                    price: 60000000000000000,
                     typeOfRoom: RoomType.SUITE,
                     roomNumber: i + 1,
-                    occupied: false
+                    occupied: false,
+                    start: 0,
+                    end: 0,
+                    owing: 0
                 });
 
                 rooms.push(room);
@@ -58,16 +70,44 @@ contract Hotel {
     // @ return RoomType typeOfRoom returns type of room
     // @ return uint roomNumber returns room number
     // @ return bool occupied returns whether room is occupied
-    function getRoom(uint index) external view returns (uint price, RoomType typeOfRoom, uint roomNumber, bool occupied) {
+    function getRoom(uint index) external view returns (uint price, RoomType typeOfRoom, uint roomNumber, bool occupied, uint start, uint end, uint owing) {
         return (
             rooms[index].price,
             rooms[index].typeOfRoom,
             rooms[index].roomNumber,
-            rooms[index].occupied
+            rooms[index].occupied,
+            rooms[index].start,
+            rooms[index].end,
+            rooms[index].owing
         );
     }
 
    // @return uint roomNumber returns room number
    // @return uint roomkey returns room key
-    function buyRoom() external returns (uint roomNumber, uint roomKey) {}
+    function bookRoom(uint index) external returns (uint roomNumber, uint roomKey) {
+        rooms[index].occupied = true;
+        rooms[index].start = block.timestamp;
+
+        roomNumber = index + 1;
+        roomKey = uint(keccak256(abi.encodePacked(rooms[index].roomNumber)));
+
+        return (roomNumber, roomKey);
+    }
+
+    function checkout(uint index) external {
+        rooms[index].end = block.timestamp;
+
+        uint numberOfDaysStayed = (rooms[index].end - rooms[index].start) / 1 days;
+        uint total = rooms[index].price * numberOfDaysStayed;
+
+        rooms[index].owing = total;
+    }
+
+    function pay(uint index) external payable {
+        require(msg.value == rooms[index].owing, "You did not pay the correct amount");
+        rooms[index].occupied = false;
+        rooms[index].start = 0;
+        rooms[index].end = 0;
+        rooms[index].owing = 0;
+    }
 }
