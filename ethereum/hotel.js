e81d9f171bd9ee971e6f199029f526c712d6fde9
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-04 21:41:08 -0500

    completed hotel instance script

---
+++ ethereum/hotel.js
@@ -0,0 +1,9 @@
+import web3 from './web3';
+import MusicList from './build/Hotel.json';
+
+const instance = new web3.eth.Contract(
+    Hotel.abi,
+    '0x6936cE1aBD283bf0789BfCb4dA4Bf530B3788AB2'
+);
+
+export default instance;
